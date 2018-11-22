1f9e2d9e929d45b559ed0c93d9e35563fce8b233
John Kirkwood <johnvkirkwood@gmail.com>
2018-11-22 10:34:37 -0500

    init

---
+++ src/firebase/init.js
@@ -0,0 +1,19 @@
+
+import firebase from 'firebase';
+//import firestore from 'firebase/firestore';
+
+// Initialize Firebase
+const config = {
+   apiKey: "AIzaSyD7XqudNQZXkdO2n6I_OqkuFwG34IxtPlQ",
+   authDomain: "chat-app-fb1e0.firebaseapp.com",
+   databaseURL: "https://chat-app-fb1e0.firebaseio.com",
+   projectId: "chat-app-fb1e0",
+   storageBucket: "chat-app-fb1e0.appspot.com",
+   messagingSenderId: "1004495103266"
+ };
+ const firebaseApp = firebase.initializeApp(config);
+ firebaseApp.firestore().settings({ timestampsInSnapshots:true });
+
+ //export firestore database
+ export default firebaseApp.firestore();
+
---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home container">
+
+    <div class="card">
+      <div class="card-content center-align">
+        <h2 class="teal-text">Welcome</h2>
+
+        <form @submit.prevent="enterChat">
+          <label for="name">Enter your name:</label>
+          <input type="text" name="name" v-model="name">
+          <button class="btn teal">Enter Chat</button>
+        </form>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+  
+  data() {
+    return {
+      name:null,
+
+    }
+  },
+
+  methods: {
+    enterChat() {
+      console.log(this.name);
+    }
+  }
+}
+</script>
